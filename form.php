2e0bdc7bcf9179c7750283ae2c36c11c2dd272f1
Azad Choubey <48096790+azadchoubey@users.noreply.github.com>
2021-02-16 18:28:48 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<link rel="stylesheet" href="sty.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div class="Heading text-center mt-4">     
+    <h5>Application Form For Joining</h5>
+    <p class="red">All the given fields are requied starts with *</p>
+</div>
+
+
+<h6 class= "bord text-center"> <strong>Part-1 Registration </strong></h6>
+<div class="form">
+<strong><p id= "ph"> Personal Details </p></strong>
+ <table>
+     <th>Name</th>
+     <td><input type="text" id="fill" requied><br>
+        <p class="red1"><u>Note:</u> Name should be same as document </p>    </td>
+
+ </table>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
